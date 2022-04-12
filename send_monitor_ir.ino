cbb4f6847bb35dd07c223003977857450be4bc11
joecalder <103054335+joecalder@users.noreply.github.com>
2022-04-11 21:03:57 -0500

    Update send_monitor_ir.ino
    added all buttons...preparing for v1 pcb

--- send_monitor_ir.ino
+++ send_monitor_ir.ino
@@ -20,30 +20,64 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 //joystick
 #define joyX A0
 #define joyY A1
-
 int xPosition = 0;
 int yPosition = 0;
 
 //buttons
-const int buttonPin = 2;     // the number of the pushbutton pin
-int buttonState = 0;         // variable for reading the pushbutton status
+const int button1Pin = 2;     // the number of the digital input pin
+int button1State = 0;         // variable for reading the pushbutton status
+const int button2Pin = 2;     // the number of the digital input pin
+int button2State = 0;         // variable for reading the pushbutton status
+const int button3Pin = 2;     // the number of the digital input pin
+int button3State = 0;         // variable for reading the pushbutton status
+const int button4Pin = 2;     // the number of the digital input pin
+int button4State = 0;         // variable for reading the pushbutton status
+const int button5Pin = 2;     // the number of the digital input pin
+int button5State = 0;         // variable for reading the pushbutton status
+const int button6Pin = 2;     // the number of the digital input pin
+int button6State = 0;         // variable for reading the pushbutton status
+const int button7Pin = 2;     // the number of the digital input pin
+int button7State = 0;         // variable for reading the pushbutton status
+const int button8Pin = 2;     // the number of the digital input pin
+int button8State = 0;         // variable for reading the pushbutton status
+const int button9Pin = 2;     // the number of the digital input pin
+int button9State = 0;         // variable for reading the pushbutton status
+const int button10Pin = 2;     // the number of the digital input pin
+int button10State = 0;         // variable for reading the pushbutton status
 
 void setup()
 {
   button1();
+  button2();
+  button3();
+  button4();
+  button5();
+  button6();
+  button7();
+  button8();
+  button9();
+  button10();
   stickup();
   stickdown();
   stickleft();
   stickright();
   receiver();
-
+  
   //IR receiver
   irrecv.enableIRIn();
   irrecv.blink13(true); //blink the board LED for debugging
 
-  //button
-  Serial.begin(9600);
-  pinMode(buttonPin, INPUT);  // initialize the pushbutton pin as an input:
+  //buttons
+  pinMode(button1Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button2Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button3Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button4Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button5Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button6Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button7Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button8Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button9Pin, INPUT);  // initialize the pushbutton pin as an input:
+  pinMode(button10Pin, INPUT);  // initialize the pushbutton pin as an input:
 
   //OLED Display
   Serial.begin(115200);
@@ -59,13 +93,22 @@ void setup()
   // Display static text @ startup
   display.println("   Welcome to your");
   display.println("");
-  display.println("    Bespoke Remote");
+  display.println("    Arcade Remote");
   display.display();
 }
 //60C08F7 menu right  60C08F7
 
 void loop() {
   button1();
+  button2();
+  button3();
+  button4();
+  button5();
+  button6();
+  button7();
+  button8();
+  button9();
+  button10();
   stickup();
   stickdown();
   stickleft();
@@ -80,7 +123,7 @@ void receiver() {
   if (irrecv.decode(&results))
   { display.clearDisplay();
     display.setCursor(0, 30);
-    display.println("Received IR"); //why does putting too much text here break this sketch?
+    display.println("Received IR"); //why does putting too much text here break this loop?
     //display.println("    Code");
     display.println(results.value, HEX);
     display.display();
@@ -90,14 +133,138 @@ void receiver() {
 }
 
 void button1() {
-  //buttons
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    // send ir code to monitor
-    irsend.sendNEC(0x60C08F7, 32);
+  button1State = digitalRead(button1Pin);
+  if (button1State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);    // send ir code
     display.clearDisplay();
     display.setCursor(0, 30);
-    display.println(" Monitor Menu Right");
+    display.println(" button 1");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button2() {
+  button2State = digitalRead(button2Pin);
+  if (button2State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button3() {
+  button3State = digitalRead(button3Pin);
+  if (button3State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button4() {
+  button4State = digitalRead(button4Pin);
+  if (button4State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button5() {
+  button5State = digitalRead(button5Pin);
+  if (button5State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button6() {
+  button6State = digitalRead(button6Pin);
+  if (button6State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button7() {
+  button7State = digitalRead(button7Pin);
+  if (button7State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button8() {
+  button8State = digitalRead(button8Pin);
+  if (button8State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button9() {
+  button9State = digitalRead(button9Pin);
+  if (button9State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
+    display.display();
+    irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
+  } else {
+    stickup();
+  }
+}
+
+void button10() {
+  button10State = digitalRead(button10Pin);
+  if (button10State == HIGH) {
+    irsend.sendNEC(0x60C08F7, 32);     // send ir code
+    display.clearDisplay();
+    display.setCursor(0, 30);
+    display.println(" button 2");
     display.display();
     irrecv.enableIRIn();//re-enable the "receive" timer since send and receive use the same timer
   } else {
